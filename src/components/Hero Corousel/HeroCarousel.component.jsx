35a160ea5c0aa738702e293618939ab87045c7dd
prakhar0211 <prakhar2nov@gmail.com>
2021-07-27 01:30:49 +0530

    add tmdb api

--- src/components/Hero Corousel/HeroCarousel.component.jsx
+++ src/components/Hero Corousel/HeroCarousel.component.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import HeroSlider from "react-slick";
 
 // component
-import {NextArrow,PrevArrow} from "./arrows.component";
+import { NextArrow, PrevArrow } from "./arrows.component";
 
 
 const HeroCarousel = () => {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => { 
+        const requestNowPlayingMovies = async () => {
+            const getImages = await axios.get("/movie/now_playing");
+            setImages(getImages.data.results)
+
+        };
+        requestNowPlayingMovies();
+        
+    }, []);
 
     const settingsLG = {
         arrows: true,
@@ -14,33 +26,23 @@ const HeroCarousel = () => {
         dots: true,
         infinite: true,
         speed: 500,
-        autoplay:true,
+        autoplay: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        nextarrow: <NextArrow/>,
-        prevarrow: <PrevArrow/>,
+        nextarrow: <NextArrow />,
+        prevarrow: <PrevArrow />,
     };
 
     const settings = {
         arrows: true,
-        dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        nextarrow: <NextArrow/>,
-        prevarrow: <PrevArrow/>,
+        nextarrow: <NextArrow />,
+        prevarrow: <PrevArrow />,
 
     }
-
-    const images = [
-        "https://images.unsplash.com/photo-1593642531955-b62e17bdaa9c?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-        "https://images.unsplash.com/photo-1626128665085-483747621778?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=610&q=80",
-        "https://images.unsplash.com/photo-1622495894321-010d1e6db2a9?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2775&q=80",
-        "https://images.unsplash.com/photo-1626050954744-92bf034ce476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
-        "https://images.unsplash.com/photo-1626074961596-cab914d9392e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=664&q=80",
-    ];
-
 
     return (
         <>
@@ -48,7 +50,7 @@ const HeroCarousel = () => {
                 <HeroSlider {...settings}>
                     {images.map((image) => (
                         <div className="w-full h-48 md:h-96 py-3">
-                            <img src={image} alt="testing" className="w-full h-full " />
+                            <img src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`} alt="testing" className="w-full h-full " />
                         </div>
                     ))}
                 </HeroSlider>
@@ -57,7 +59,7 @@ const HeroCarousel = () => {
                 <HeroSlider {...settingsLG}>
                     {images.map((image) => (
                         <div className="w-full h-80 px-1.5 py-3 ">
-                            <img src={image} alt="testing" className="rounded w-full h-full" />
+                            <img src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`} alt="testing" className="rounded w-full h-full" />
                         </div>
                     ))}
                 </HeroSlider>
